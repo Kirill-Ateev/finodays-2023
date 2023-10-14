458ff4b9525f93eb341fa0323d716bbd8cc32841
Kirill Ateev <g810bAKO@yandex.ru>
2023-10-14 20:24:36 +0300

    feat: fetching gold price from oracle

---
+++ blockchain/contracts/GoldOracle.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GoldOracle is Ownable {
+    uint256 private priceGold;
+
+    constructor(address initialOwner) Ownable(initialOwner) {}
+
+    function setPriceGold(uint256 _newPrice) public onlyOwner {
+        priceGold = _newPrice;
+    }
+
+    function getPriceGold() public view returns (uint256) {
+        return priceGold;
+    }
+}
